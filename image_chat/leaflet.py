7a27382717f0ff08083ed40e923ff344f2d8a603
trantunhi99 <trantunhi99@gmail.com>
2025-10-22 17:47:05 -0500

    test

--- image_chat/leaflet.py
+++ image_chat/leaflet.py
@@ -177,7 +177,7 @@ def create_leaflet_map(
             )
 
             colorbars.append(cb)
-            offset += 30  # pixel spacing between colorbars
+            offset += 5  # pixel spacing between colorbars
 
     # create map
     # PZhang added this (deal with empty input regions)
